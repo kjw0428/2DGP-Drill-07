d140f77957b40064ee20bba479e4e679d94c3228
kjw0428 <naver.com@tukorea.ac.kr>
2025-09-30 23:17:08 +0900

    initial commit

---
+++ boy_grass_object.py
@@ -0,0 +1,112 @@
+from pico2d import *
+import random
+
+
+# Game object class here
+#1. 객체를 도출 - 추상화
+#2. 속성을 도출 - 추상화
+#3. 행위를 도출
+#4. 클래스를 제작
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 599
+        self.frame = random.randint(0, 7)
+        self.image = load_image('ball21x21.png')
+
+    def update(self):  # 객체의 상호 작용. 행위
+        self.frame = (self.frame + 1) % 8
+        self.y -= 5
+
+    def draw(self): #객체의 상호 작용. 행위
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+        pass
+
+class Zombie:
+    def __init__(self):
+        self.x, self.y = 100, 170
+        self.frame = 0
+        self.image = load_image('zombie_run_animation.png')
+    def update(self):
+        self.frame = (self.frame + 1) % 10
+        self.x += 5
+    def draw(self):
+        frame_width = self.image.w // 10
+        frame_height = self.image.h
+        self.image.clip_draw(self.frame * frame_width, 0, frame_width, frame_height,
+                            self.x, self.y, frame_width // 2, frame_height // 2)
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def update(self):  # 객체의 상호 작용. 행위
+        pass
+
+    def draw(self): #객체의 상호 작용. 행위
+        self.image.draw(400, 30)
+        pass
+
+
+
+
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+
+
+
+
+
+def reset_world():
+    global running
+    global grass
+    global boy
+    global world
+
+    global team
+    world = []
+    team = [Boy() for i in range(20)]
+    world += team
+
+    running = True
+
+    grass = Grass()
+    world.append(grass)
+    boy = Boy()
+
+    zombie = Zombie()
+    world.append(zombie)
+
+def update_world():
+    for o in world:
+        o.update()
+
+    pass
+
+
+def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
+    pass
+
+open_canvas()
+
+reset_world()
+
+while True:
+    handle_events()
+    update_world() # 객체들의 상호 작용을 시뮬레이션, 계산
+    render_world() # 객체들의 모습을 그린다.
+    delay(0.05)
+
+close_canvas()
